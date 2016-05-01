117407ec10c84f024aa6b50245a40c61ef66c3e8
Susan <susanschrum@mgail.com>
2016-05-01 08:29:53 -0400

    Removed sidebar from Contact Page, added color to <a> tag

---
+++ wp-content/themes/sks-portfolio/page-contact-me.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<div id="content">
+
+	<div id="inner-content" class="wrap cf">
+
+		<main id="main" class="m-all t-all d-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+					<header class="article-header">
+						<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+					</header> <?php // end article header ?>
+
+					<section class="entry-content cf" itemprop="articleBody">
+						<?php the_content();?>
+					</section> <?php // end article section ?>
+
+					<footer class="article-footer cf">
+
+					</footer>
+
+				</article>
+
+			<?php endwhile; endif; ?>
+
+		</main>
+	</div>
+</div>
+
+<?php get_footer(); ?>
