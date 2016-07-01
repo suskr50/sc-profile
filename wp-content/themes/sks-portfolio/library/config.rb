6431afee391b6e07aba75a24d36f0cf38ed2b13f
Susan Schrum <susanschrum@gmail.com>
2016-07-01 07:49:28 -0400

    Minimized css

--- wp-content/themes/sks-portfolio/library/config.rb
+++ wp-content/themes/sks-portfolio/library/config.rb
@@ -2,7 +2,7 @@
 css_dir = "css" # by Compass.app 
 sass_dir = "scss" # by Compass.app 
 images_dir = "images" # by Compass.app 
-output_style = :expanded # by Compass.app 
+output_style = :compressed # by Compass.app 
 relative_assets = false # by Compass.app 
 line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
